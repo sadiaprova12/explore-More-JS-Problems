22c6a375b31f5ffab0ef2d33531ca4e6395a0eb7
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-04 19:04:19 +0600

    type of add

--- array.js
+++ array.js
@@ -3,9 +3,12 @@ const age = 53;
 const isIndependent = true;
 const student = {id: 121, class: 11, name: 'Agun'};
 const friends = [13, 14, 11, 17, 21, 16, 15, 20];
-
+function add(num1, num2){
+    return num1 + num2;
+}
 console.log(typeof country);
 console.log(typeof age);
 console.log(typeof isIndependent);
 console.log(typeof student);
 console.log(typeof friends);
+console.log(typeof add);
