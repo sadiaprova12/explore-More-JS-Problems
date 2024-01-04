135bae71094b829f8a0ded01912374bfea14e1f9
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-04 22:20:06 +0600

    all slice

--- slice.js
+++ slice.js
@@ -4,3 +4,4 @@ const friends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
 // 2 er age 5 r age j maan thakbe se gulo pabo
 const partial = friends.slice(2, 5);
 console.log(partial);
+console.log(friends);
