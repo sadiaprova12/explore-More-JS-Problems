3a51501981800b5e7129eb59678f861b02237eca
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-04 18:53:33 +0600

    added array js

---
+++ array.js
@@ -0,0 +1,7 @@
+const country = 'Bangladesh';
+const age = 53;
+const isIndependent = true;
+const student = {id: 121, class: 11, name: 'Agun'};
+const friend = [13, 14, 11, 17, 21, 16, 15, 20];
+
+console.log(typeof country);
