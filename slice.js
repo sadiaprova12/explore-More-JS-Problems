58df78ff3909a5caf8e1c6252432d93894f25193
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-04 22:17:38 +0600

    array slice 2 to 5

--- slice.js
+++ slice.js
@@ -1,3 +1,6 @@
 // Get Part Of An Array And Insert Elements Using Slice, Splice
 
 const friends = [12, 45, 32, 22, 44, 62, 29, 69, 87];
+// 2 er age 5 r age j maan thakbe se gulo pabo
+const partial = friends.slice(2, 5);
+console.log(partial);
