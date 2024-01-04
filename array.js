03e6109f214bd5c0851383994f5cf0729965621c
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-04 19:01:41 +0600

    type of student and friends

--- array.js
+++ array.js
@@ -2,8 +2,10 @@ const country = 'Bangladesh';
 const age = 53;
 const isIndependent = true;
 const student = {id: 121, class: 11, name: 'Agun'};
-const friend = [13, 14, 11, 17, 21, 16, 15, 20];
+const friends = [13, 14, 11, 17, 21, 16, 15, 20];
 
 console.log(typeof country);
 console.log(typeof age);
 console.log(typeof isIndependent);
+console.log(typeof student);
+console.log(typeof friends);
