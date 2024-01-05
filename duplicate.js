bf959fb3acae493bb56bd5d3a41e781f663fa7e3
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-05 20:02:20 +0600

    added duplicate js

---
+++ duplicate.js
@@ -0,0 +1,10 @@
+const names = ['abul', 'babul', 'cabul', 'dabul', 'ebul', 'babul', 'abul', 'kabul', 'gabul', 'cabul', 'baul', 'abul', 'abul'];
+
+function removeDuplicate(names){
+    for(let i = 0; i < names.length; i++){
+        const name = names[i];
+        console.log(name);
+    }
+}
+
+const uniquesNames = removeDuplicate(names);
