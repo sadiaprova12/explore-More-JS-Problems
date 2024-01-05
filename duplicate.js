ab9fd451e83b46bc6a140ea67e23d6ed9d4ad4ea
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-05 20:13:52 +0600

    Remove Duplicate Items From An Array

--- duplicate.js
+++ duplicate.js
@@ -1,10 +1,16 @@
 const names = ['abul', 'babul', 'cabul', 'dabul', 'ebul', 'babul', 'abul', 'kabul', 'gabul', 'cabul', 'baul', 'abul', 'abul'];
 
 function removeDuplicate(names){
+    const unique = [];
     for(let i = 0; i < names.length; i++){
         const name = names[i];
-        console.log(name);
+        if(unique.includes(name) === false){
+            unique.push(name);
+        }
+        // console.log(name);
     }
+    return unique;
 }
 
 const uniquesNames = removeDuplicate(names);
+console.log(uniquesNames);
