4315a434e507f95e417de66d6890daa7f1fa22cb
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-06 22:52:03 +0600

    error message and multiply

--- validaction.js
+++ validaction.js
@@ -6,5 +6,14 @@ function add(num1, num2){
     return num1 + num2
 }
 
-const result = add(12, '45');
-console.log(result);
+// const result = add(12, '45');
+const result = add(false, 45); // Error message return diye dibe ei line dile
+// console.log(result);
+
+function multiply(num1, num2){
+    console.log(num1, num2);
+    return num1 * num2;
+}
+
+const output = multiply(12,45);
+console.log(output);
