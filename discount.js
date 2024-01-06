40369c380548601b3def0db9a3dce404a6afde41
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-06 22:33:53 +0600

    total cost

--- discount.js
+++ discount.js
@@ -23,7 +23,14 @@
         const totalPrice = first100Price + restTicketPrice;
         return totalPrice;
     }
-
+    else{
+        const first100Price = 100 * first100Rate;
+        const second100Price = 100 * second100Rate;
+        const restTicketQuantity = number - 200;
+        const restTicketPrice = number * restTicketRate;
+        const totalCost = first100Price + second100Price + restTicketPrice;
+        return totalCost;
+    }
 }
 
 const price = ticketPrice(120);
