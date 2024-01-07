6e9d240a38ee0caecc1c65a04aa7305d012ad6ea
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-07 10:59:15 +0600

    array like object

---
+++ arguments.js
@@ -0,0 +1,9 @@
+// Explore What You Can Do With JavaScript Object
+
+function add(num1, num2){
+    console.log(num1, num2);
+    // console.log(arguments);
+    console.log(arguments[4]);
+}
+
+add(12, 13, 45, 89, 78); // 100% array na Array like object bola hoy
