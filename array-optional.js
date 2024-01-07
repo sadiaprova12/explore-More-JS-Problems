b5718d0f339e2bc5d29196f34ffa0630e297fcc8
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-07 10:55:41 +0600

    array

---
+++ array-optional.js
@@ -0,0 +1,17 @@
+// Explore What You Can Do With JavaScript Object
+
+// const products = [
+//     {name: 'phone', price: 12000},
+//     {name: 'laptop', price: 32000},
+// ]
+
+// products[0];
+// products[1];
+
+//const products = [15, 56, 87]
+
+const products = {
+    '0': 15,
+    '1': 57,
+    '2': 86,
+}
