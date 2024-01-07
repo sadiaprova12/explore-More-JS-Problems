100af5d08ea6d80b67c6ea417fa7eb13532b88a9
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-07 10:19:04 +0600

    stoping condition

--- simple.js
+++ simple.js
@@ -1,8 +1,24 @@
 // Understand recursion using sum of numbers
 
-let sum = 0;
-for(let i = 1; i <= 5; i++){
-    sum = sum + i;
-}
+// let sum = 0;
+// for(let i = 1; i <= 5; i++){
+//     sum = sum + i;
+// }
 
-console.log(sum);
+// console.log(sum);
+
+// let sum = 0;
+// for(let i = 5; i >= 1; i++){
+//     sum = sum + i;
+// }
+// // console.log(sum);
+
+function sum(i){
+    if(i==1){ // stoping condition
+        return 1;
+    }
+    console.log(i);
+    return i + sum(i-1);
+}
+const result = sum(5);
+console.log(result);
