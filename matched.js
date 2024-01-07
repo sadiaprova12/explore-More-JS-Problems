cc6d1532fecf8fca7107f1b02991ae71a70807dc
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-07 14:23:42 +0600

    product

--- matched.js
+++ matched.js
@@ -9,3 +9,18 @@ const numbers = [45, 65, 23, 98, 19];
 for(const number of numbers){
    console.log(number);
 }
+
+const products = [
+    {id: 1, name: 'Xiami phone one night', price: 19000},
+    {id: 1, name: 'Iphone', price: 19000},
+    {id: 1, name: 'mac book air', price: 19000},
+    {id: 1, name: 'lenovo yoga laptop 2025', price: 19000},
+    {id: 1, name: 'Dell insprision laptop', price: 19000},
+    {id: 1, name: 'Samsung phone note 7', price: 19000},
+    {id: 1, name: 'Nokia old age phone gone', price: 19000},
+    {id: 1, name: 'Phone one', price: 19000},
+];
+
+for(const product of products){
+    console.log(product);
+}
