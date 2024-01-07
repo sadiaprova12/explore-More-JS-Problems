b089b5a691605c2f19dfcdf135b3913c565a5ad9
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-07 10:27:00 +0600

    calculate sum of numbers using recursion

--- simple.js
+++ simple.js
@@ -22,3 +22,26 @@ function sum(i){
 }
 const result = sum(5);
 console.log(result);
+
+// Explore Factorial Recursion Using A For Loop Concept
+
+// Recursion 
+/**
+  5 + sum(4)
+  5 + i + sum(i-1)
+  5 + 4 + sum (4-1) = 3
+  5 + 4 + 3 + sum (3-1) = 2
+  5 + 4 + 3 + 2 + sum (2-1) = 1
+  5 + 4 + 3 + 2 + 1
+ */
+
+  // Calculate sum of numbers using recursion
+
+  function sum(i){
+    if (i == 1 ){
+        return 1;
+    }
+    return i + sum(i - 1);
+  }
+  const cost = sum(5);
+  console.log(cost);
